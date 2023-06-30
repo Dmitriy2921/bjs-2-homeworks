3604b62e6f9da2d6541492890dad0efe871e79cb
Dmitriy2921 <Dimka294@list.ru>
2023-06-30 17:20:48 +0300

    Выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -7,8 +7,8 @@ function cachingDecoratorNew(func) {
     const idx = cache.findIndex((item) => hash === item.hash);
 
     if (idx !== -1) {
-      console.log("Из кэша: " + cache[idx].value);
-      return "Из кэша: " + cache[idx].value;
+      console.log("Из кеша: " + cache[idx].value);
+      return "Из кеша: " + cache[idx].value;
     }
 
     let result = func(...args);
