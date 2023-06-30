fd077262d7f8b639380be75b9352476d09ec3c1c
Dmitriy2921 <Dimka294@list.ru>
2023-06-30 16:46:10 +0300

    Выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,48 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+    const idx = cache.findIndex((item) => hash === item.hash);
+
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    }
+
+    let result = func(...args);
+    cache.push({hash: hash, value: result});
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+
+  }
+  return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+ 
+  function wrapper(...args) {
+
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      func(...args);
+    }, delay);
+
+    wrapper.allCount++;
+  }
+
+  wrapper.allCount = 0;
+  wrapper.count = 0;
+  return wrapper;
 }
